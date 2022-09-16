1f7d329148d5819d4edd4cab973d020f6a2f882a
Priyanka_Hatgine <patil.priyanka885@gmail.com>
2022-09-16 16:48:59 +0530

    Solved Que 7,8

---
+++ src/main/java/main/Example07.java
@@ -0,0 +1,69 @@
+package main;
+
+public class Example07 {
+    public static void main(String[] args) {
+        Example07 example = new Example07();
+        int[] firstArray = {1, 2, 3,};
+        int[] secondArray = {1, 2, 3, 4, 5};
+        if (Example07.isEqual(firstArray, secondArray)) {
+            System.out.println("First array is equal to Second array ");
+        } else if (Example07.isSublist(firstArray, secondArray)) {
+            System.out.println("First array is a sublist of Second array ");
+        } else if (Example07.isSuperlist(firstArray, secondArray)) {
+            System.out.println("First array is a superlist of Second array ");
+        } else {
+            System.out.println("First array is not a superlist of, sublist of or equal to Second array\n");
+        }
+    }
+
+    private static boolean isSublist(int[] firstArray, int[] secArray) {
+        int j;
+        if (firstArray.length > secArray.length) {
+            return false;
+        } else {
+            for (int i = 0; i < firstArray.length; i++) {
+                for (j = 0; j < secArray.length; j++) {
+                    if (firstArray[i] == secArray[j])
+                        break;
+                }
+                if (j == secArray.length)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isSuperlist(int[] firstArray, int[] secArray) {
+        int j;
+        if (firstArray.length < secArray.length) {
+            return false;
+        } else {
+            for (int i = 0; i < secArray.length; i++) {
+                for (j = 0; j < firstArray.length; j++) {
+                    if (firstArray[j] == secArray[i])
+                        break;
+                }
+                if (j == secArray.length)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isEqual(int[] firstArray, int[] secArray) {
+        if (firstArray.length != secArray.length) {
+            return false;
+        } else {
+            int j;
+            for (int i = 0; i < firstArray.length; i++) {
+                for (j = 0; j < secArray.length; j++) {
+                    if (secArray[j] == firstArray[i])
+                        break;
+                }
+                if (j == firstArray.length)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/main/Example08.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Example08 {
+    public static void main(String[] args) {
+        Example08 example = new Example08();
+        final String givenWord = "Machine";
+        boolean result = Example08.isIsogram(givenWord);
+        if (result) {
+            System.out.println(givenWord + " is an isogram");
+        } else {
+            System.out.println(givenWord + " is not isogram");
+        }
+    }
+
+    private static boolean isIsogram(String word) {
+        Set<Character> set = new HashSet<>();
+        word = word.toLowerCase();
+        char[] arr = word.toCharArray();
+        for (char ch : arr) {
+            set.add(ch);
+        }
+        if (set.size() == word.length())
+            return true;
+        else
+            return false;
+    }
+}
