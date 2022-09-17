a4fc6f0c9e90c14f4d4d9675dda98b1057b5a0e0
Priyanka_Hatgine <patil.priyanka885@gmail.com>
2022-09-17 19:31:49 +0530

    Added Junit for read file data

--- src/test/java/com/cognologix/collection/utils/TestFileHandling.java
+++ src/test/java/com/cognologix/collection/utils/TestFileHandling.java
@@ -1,11 +1,24 @@
 package com.cognologix.collection.utils;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
 
 public class TestFileHandling {
 
     @Test
     public void testReadFileData() {
-
+        List<String> stringList = null;
+        try {
+            stringList = FileHandling.readFileData("/data/SoftwareData.txt");
+            Assertions.assertEquals(6, stringList.size());
+            for (String str : stringList) {
+                Assertions.assertNotEquals(null, str);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
